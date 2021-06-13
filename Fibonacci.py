f79349a104fb6cfe069e72e87511e1b5d7f2c556
Balaji-0-5 <84007002+Balaji-0-5@users.noreply.github.com>
2021-06-13 12:40:31 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,9 +2,12 @@ limit = int(input("Enter the required number of terms in the Fibonacci series : 
 i = -1
 j = 1
 n = 0
-while n < limit:
-    k = i + j
-    print(k)
-    i = j
-    j = k
-    n += 1
+if limit <= 0:
+    print("Please enter a positive integer greater than 0")
+else:
+    while n < limit:
+        k = i + j
+        print(k)
+        i = j
+        j = k
+        n += 1
